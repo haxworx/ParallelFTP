83cc34b7e9a951eb85fbcd2888c06000a530d172
Al Poole <al@Workstation.home>
2015-06-01 17:53:21 +0100

    got rid of stuffz

--- ParallelFTP.pl
+++ ParallelFTP.pl
@@ -1,6 +1,5 @@
 #! /usr/bin/perl
 
-
 #
 #  Copyright (c) 2015, Al Poole <netstar@gmail.com>
 #
@@ -19,6 +18,125 @@
 
 #  I once worked for a company whih used FTP to transfer loads of files
 #  concurrently. For phun I wrote this using an untested Transfer.pm
+
+
+use strict;
+use warnings;
+
+use Net::FTP;
+use Net::SCP qw/scp iscp/;
+use LWP::UserAgent;
+
+package Transfer;
+
+sub new {
+	my ($self, $hostname, $username, $password) = @_;
+	
+	my $CLASS = "";
+
+	if ($hostname =~ m/\Aftp:\/\/(.+)\z/) {
+		$hostname = $1;
+		$CLASS = 'Net::FTP';
+	} elsif ($hostname =~ m/\Ascp:\/\/(.+)\z/) {
+		$hostname = $1;
+		$CLASS = 'Net::SCP';
+	} elsif ($hostname =~ m/\Ahttps?:\/\/(.+)\z/) {
+		$hostname = $1;
+		$CLASS = 'LWP::UserAgent';
+	} else {
+		die "Unknown protocol prefix!";
+	}
+
+	my %args = (
+		'Host' => $hostname,
+		'user' => $username,
+		'host' => $hostname,
+		'password' => $password,
+		'cwd' => '.',
+		'Debug'  => 0,
+		'type' => $CLASS,
+	);
+
+	if ($CLASS eq "Net::SCP") {
+		$args{'handle'} = $CLASS->new(\%args) || 	
+				die "new()". $args{'handle'}->{errstr} . "\n";
+	} elsif ($CLASS eq "Net::FTP") {
+		$args{'handle'} = $CLASS->new($hostname);
+		$args{'handle'}->login($username, $password) ||
+                        die "login() $args{'handle'}->{errstr}";
+	} elsif ($CLASS eq "LWP::UserAgent") {
+		$args{'handle'} = $CLASS->new() ||
+			die "UserAgent()";
+	}
+	
+	return bless \%args;
+}
+
+sub folder {
+	my $self = shift;
+	return $self->{'folder'};
+}
+
+sub username {
+	my $self = shift;
+	return $self->{'username'};
+}
+
+sub password {
+	my $self = shift;
+	return $self->{'password'};
+}
+sub handle {
+	my $self = shift;
+	return bless $self->{'handle'}, $self->{'type'};
+}
+
+sub type {
+	my $self = shift;
+	return $self->{'type'};
+}
+
+sub post {
+	my ($self, %args) = @_;
+
+	my $url = $self->hostname();
+	if (!defined $url) {
+		$url = $args{'url'};
+	}
+		
+	my $response = $self->handle->post($url,	
+		[ 'name'  => $args{'name'}, 
+		  'value' => $args{'value'}
+		]
+	);
+
+	die "post()\n" unless $response->is_success;
+}
+
+sub put_files {
+	my ($self, @file_list) = @_;	
+
+	foreach (@file_list) {
+		$self->handle->put($_) || die "put()";
+	}
+
+	$self->handle->close();
+}
+
+sub get_files {
+	my ($self, @file_list) = @_;
+
+	foreach (@file_list) {
+		$self->handle->get($_);
+	}
+
+	$self->handle->close();
+}
+
+1;
+
+package main;
+
 
 
 sub GetFilesList {
@@ -78,8 +196,6 @@ sub GetCPUCount {
 
 # FTP Parallel
 
-use Transfer;
-
 sub main {
 	my ($hostname, $username, $password, $directory) = @_;
 
@@ -106,11 +222,10 @@ sub main {
 		}
 	}
 
-	foreach (@pid) {
+	foreach (@pids) {
 		waitpid($_, -1);
 	}
-
-	print "You have $cpu_count CPUs\n"; exit;
+	print "You have $cpu_count CPUs\n"; 
 	print "done!";
 }
 
